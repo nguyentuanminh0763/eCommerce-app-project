cfe46d467387eb824978c9253c0d8912e6cd3eaf
Minh Nguyen <draculedolar0763@gmail.com>
2025-07-01 13:15:50 +0700

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const HomeScreen = (props: Props) => {
+   console.log("HomeScreen");
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
---
+++ app/index.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+type Props = {};
+
+const WelcomeScreen = (props: Props) => {
+  console.log("WelcomeScreen");
+  
+  return (
+    
+    <View style={styles.container}>
+      <Text>Welcome Screen</Text>
+      <Link href={"/signin"} asChild>
+        <TouchableOpacity>
+          <Text>Go to SignIn Screen</Text>
+        </TouchableOpacity>
+      </Link>
+      <Link href={"/signup"} asChild>
+        <TouchableOpacity>
+          <Text>Go to SignUp Screen</Text>
+        </TouchableOpacity>
+      </Link>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
